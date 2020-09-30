567cf8bbb85667615166b8aa3a7f32d2094f9d7b
James Conallen <jconallen@gmail.com>
2020-09-30 13:15:21 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# coc-truck.temp-mon
-Simple temperature monitoring service for IEAM demo
+# Simple temperature monitoring service for IEAM demo
+
+This is a simple service that is managed and deployed by the 
+IBM Edge Application Management.  This demo application simulates 
+the collection and reporting of temperature values in a delivery 
+truck.
+
+
---
+++ app.js
@@ -0,0 +1,31 @@
+const request = require('request-json');
+const random = require('random');
+const fs = require('fs');
+
+const appVersion = "1.0.2";
+
+const DEVICE = process.env.HZN_DEVICE_ID;
+const HUB_URL = process.env.HUB_URL;
+
+
+log(`Version: ${appVersion}`  );
+log(`Hub: ${process.env.HUB_URL}`);
+
+var client = request.createClient(HUB_URL);
+ 
+function update(){
+
+    var temp = (random.int(min = 270, max = 320))/10;
+
+    var data = {
+        "id": DEVICE,
+        "temp": temp
+    }
+
+    client.post('data/', data, function(err, res, body) {
+        return console.log("POST /data/ " + JSON.stringify(data) + ' response: ' + res.statusCode);
+    });
+
+}
+
+setInterval(update,30000);
